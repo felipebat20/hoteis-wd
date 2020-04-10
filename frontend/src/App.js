3a737bf58c0a9fe75022686af000b51db466051b
felipebatista24680@gmail.com <felipebatista24680@gmail.com>
2020-04-10 06:14:59 -0300

    updating files

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+import Main from './pages/Main/index';
+const App = () => (
+  <div className="App">
+    <Header/>
+    <Main/>
+  </div>
+);
+
+
+
+export default App;
---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">Hoteis TimeToTravel</header>
+);
+
+export default Header;
