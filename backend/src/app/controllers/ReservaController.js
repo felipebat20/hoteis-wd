4f38e284f83a39f91e519d7351b8efa12b7aa934
felipebatista24680@gmail.com <felipebatista24680@gmail.com>
2020-05-11 22:25:17 -0300

    ReservaControl populate

--- backend/src/app/controllers/ReservaController.js
+++ backend/src/app/controllers/ReservaController.js
@@ -25,6 +25,7 @@ class ReservaController {
             return res.status(401).json({ Error: "Hotel not found" });
 
         const reserva = await Reserva.create({ responsavel: user_id, hotel: hotel_id, dataInicial, dataFinal, qtdeHospedes });
+        await reserva.populate('User').populate('hotel').execPopulate();
         return res.json(reserva);
     }
     async index(req, res) {
