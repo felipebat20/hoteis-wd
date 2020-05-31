5311c06064349f4927e5f9c201832f7d804995b3
felipebatista24680@gmail.com <felipebatista24680@gmail.com>
2020-05-31 00:07:01 -0300

    populate in the paginate

--- backend/src/app/controllers/ReservaController.js
+++ backend/src/app/controllers/ReservaController.js
@@ -37,9 +37,15 @@ class ReservaController {
         if (!user)
             return res.status(401).json({ error: "User not found." });
         const { page = 1 } = req.query;
-        const reserva = await Reserva.paginate({}, { page, limit: 10 });
+        const options = {
+            page: 1,
+            limit: 10,
+            populate: 'responsavel hotel'
+        }
+        const reserva = await Reserva.paginate({ responsavel: user_id }, options);
         return res.json(reserva);
     }
+
 }
 
 export default new ReservaController();
