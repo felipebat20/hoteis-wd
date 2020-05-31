5db70810c4be837bdedbadb3ff279e7a0ae5b119
felipebatista24680@gmail.com <felipebatista24680@gmail.com>
2020-05-31 01:06:13 -0300

    Yup validation

--- backend/src/app/controllers/ReservaController.js
+++ backend/src/app/controllers/ReservaController.js
@@ -3,9 +3,21 @@ import mongoose from 'mongoose';
 import Reserva from '../models/Reserva';
 import User from '../models/User';
 import Hotel from '../models/Hotel';
+import * as yup from 'yup';
 
 class ReservaController {
     async store(req, res) {
+
+        const reservaSchema = yup.object().shape({
+            qtdeHospedes: yup.number().required('Number is required'),
+            dataInicial: yup.date().required('Start date is required'),
+            dataFinal: yup.date().min(yup.ref('dataInicial'), 'End date should be later than start date').required('End date is required'),
+
+        });
+
+        if (!(await reservaSchema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
         const { user_id } = req.headers;
         const { hotel_id } = req.params;
         const { dataInicial, dataFinal, qtdeHospedes } = req.body;
@@ -29,6 +41,7 @@ class ReservaController {
 
         return res.json(reserva);
     }
+
     async index(req, res) {
         const { user_id } = req.params;
         const user = await User.findOne({ _id: user_id }).catch(err => {
